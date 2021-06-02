bb8399c6dc4a9696aadab50a05d210c4bb1a202c
Adrien75-lab <adrichristophe@gmail.com>
2021-06-02 12:26:05 +0200

    ajout d'images

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,23 +5,48 @@ function Home() {
   return (
     <div className="home">
       <div class="container">
-       <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src={ require("../assets/img/cascades.jpg").default } alt="" className="img-fluid"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        
+        <div class="col-lg-5">
+          <h1 class="font-weight-light">Bienvenu en Island</h1>
+          <p>Venez Découvrir l'Island à travers un album photo</p>
+        </div>
+
+        <img
+          class="img-fluid rounded mb-4 mb-lg-0"
+          src={require("../assets/img/cascades.jpg").default}
+          alt=""
+          className="img-fluid"
+          alt=""
+        />
+
+        <img
+          class="img-fluid rounded mb-4 mb-lg-0"
+          src={require("../assets/img/island.jpg").default}
+          alt=""
+          className="img-fluid1"
+          alt=""
+        />
+        <img
+          class="img-fluid rounded mb-4 mb-lg-0"
+          src={require("../assets/img/island2.jpg").default}
+          alt=""
+          className="img-fluid1"
+          alt=""
+        />
+        <img
+          class="img-fluid2 rounded mb-4 mb-lg-0"
+          src={require("../assets/img/ilsand3.jpg").default}
+          alt=""
+          className="img-fluid2"
+          alt=""
+        />
+        <img
+          class="img-fluid2 rounded mb-4 mb-lg-0"
+          src={require("../assets/img/ilsand3.jpg").default}
+          alt=""
+          className="img-fluid2"
+          alt=""
+        />
+
       </div>
     </div>
   );
